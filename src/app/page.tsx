5a9e27db545b95eeb99c5f3b0c5ce4a2402bf30f
Larisa Novozhilova <kara4516@gmail.com>
2024-08-29 16:30:51 +0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
+'use client';
+import { useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/hooks/use-auth';
+import { Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
-export default function Home() {
+export default function Page() {
+  const { isAuth, email } = useAuth();
+  const router = useRouter();
+  let username = '';
+
+  if (email) {
+    username = email!.split('@')[0];
+  }
+
+  useEffect(() => {
+    if (isAuth) {
+      router.push('/');
+    }
+  }, [isAuth, router]);
+
   return (
-    <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-      This is an example of using MUI component
-    </Typography>
+    <main className="main">
+      <div className="container">
+        <Stack direction="column" justifyContent="space-between" alignItems="center" spacing={2}>
+          <Typography variant="h3" component="p">
+            Welcome Back, {username}!
+          </Typography>
+          <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+            <Link href="/rest">REST Client</Link>
+            <Link href="/graphiql">GraphiQL Client</Link>
+            <Link href="/history">History</Link>
+          </Stack>
+        </Stack>
+      </div>
+    </main>
   );
 }
