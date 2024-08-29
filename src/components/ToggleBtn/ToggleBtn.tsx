e98651327ee8df573334bd70a43199fa6aa10da1
Larisa Novozhilova <kara4516@gmail.com>
2024-08-29 16:29:56 +0300

    feat: create btn component

---
+++ src/components/ToggleBtn/ToggleBtn.tsx
@@ -0,0 +1,19 @@
+'use client';
+import * as React from 'react';
+import { ToggleButton } from '@mui/material';
+import { ToggleButtonGroup } from '@mui/material';
+
+export default function ColorToggleButton() {
+  const [alignment, setAlignment] = React.useState('en');
+
+  const handleChange = (event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup color="primary" value={alignment} exclusive onChange={handleChange} aria-label="Platform">
+      <ToggleButton value="en">EN</ToggleButton>
+      <ToggleButton value="ru">RU</ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
