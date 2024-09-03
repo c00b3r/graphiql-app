7d9f681f18db79e76ef273425c2d296b326cfed7
Larisa Novozhilova <kara4516@gmail.com>
2024-09-03 18:40:37 +0300

    feat: add log for errors

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { setLogLevel } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,6 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+setLogLevel('error');
 
 export const auth = getAuth(app);
 
