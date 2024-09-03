8fb58f7973bbfd5f37e3b37433c0264decc8ee9a
Larisa Novozhilova <kara4516@gmail.com>
2024-09-03 18:41:44 +0300

    fix: add routing for welcome page

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,9 +1,27 @@
 'use client';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+import Loader from '@/components/Loader/Loader';
 
 export default function WelcomePage() {
+  const router = useRouter();
+  const isAuth = useSelector((state: RootState) => state.user.isAuthenticated);
+
+  useEffect(() => {
+    if (isAuth) {
+      router.push('/');
+    }
+  }, [isAuth, router]);
+
+  if (isAuth) {
+    return <Loader />;
+  }
+
   return (
     <main className="main">
       <div className="container">
@@ -12,8 +30,12 @@ export default function WelcomePage() {
             Welcome!
           </Typography>
           <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-            <Link href="/login">Sing In</Link>
-            <Link href="/signup">Sing Up</Link>
+            <Button variant="contained" size="large" component={Link} href="/login">
+              Sign In
+            </Button>
+            <Button variant="outlined" size="large" component={Link} href="/signup">
+              Sign Up
+            </Button>
           </Stack>
         </Stack>
       </div>
