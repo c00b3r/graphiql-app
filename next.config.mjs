000968d747763c84ab0bb8c81793961446e056c9
c00b3r <sanuyOKmaklaren0>
2024-09-16 04:42:58 +0500

    feat: basePath: ''

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -16,7 +15,7 @@ const nextConfig = {
       },
     ],
   },
-  basePath: '/',
+  basePath: '',
   reactStrictMode: true,
 };
 
