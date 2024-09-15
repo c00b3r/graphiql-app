4332ae352fc7d496207ddf55af7bd0d9a256c1c1
HunterTigerX <HunterTigerX@gmail.com>
2024-09-15 20:22:52 +0300

    feat: updated tests

--- src/components/graphql/ResponseGQL.test.tsx
+++ src/components/graphql/ResponseGQL.test.tsx
@@ -1,26 +1,67 @@
-import { describe, it, vi } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '@/reducers/root/rootReduces';
-import ResponseGQL from './ResponseGQL';
+import { legacy_createStore as createStore } from 'redux';
+import ResponseGQL from './ResponseGQL'; // Убедитесь, что путь правильный
 
-vi.mock('next/navigation', () => ({
-  useRouter: vi.fn(),
-}));
-vi.mock('next/router', () => ({
-  useRouter: vi.fn(),
-}));
-
-const MockResponseGQL = () => {
-  return (
-    <Provider store={store}>
-      <ResponseGQL />
-    </Provider>
-  );
+const mockState = {
+  main: {
+    searchResults: {
+      result: '{"key": "value"}',
+      code: 200,
+      from: true,
+    },
+    documentation: true,
+    languageData: {
+      responseHeader: 'Response Header',
+      status: 'Status',
+      body: 'Body',
+    },
+  },
 };
 
-describe('Search Component', () => {
-  it('renders without crashing', () => {
-    render(<MockResponseGQL />);
+const createMockStore = (initialState: unknown) => {
+  return createStore((state = initialState) => state);
+};
+
+describe('ResponseGQL Component', () => {
+  test('renders response correctly when code is 200', () => {
+    const store = createMockStore(mockState);
+
+    render(
+      <Provider store={store}>
+        <ResponseGQL />
+      </Provider>
+    );
+
+    expect(screen.getByText(mockState.main.languageData.responseHeader)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockState.main.languageData.status}: ${mockState.main.searchResults.code}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(`${mockState.main.languageData.body}:`)).toBeInTheDocument();
+    expect(screen.getByText('Response Header')).toBeInTheDocument();
+  });
+
+  test('renders error message when code is not 200', () => {
+    const errorState = {
+      ...mockState,
+      main: {
+        ...mockState.main,
+        searchResults: {
+          ...mockState.main.searchResults,
+          code: 404,
+          message: 'Not Found',
+        },
+      },
+    };
+
+    const store = createMockStore(errorState);
+
+    render(
+      <Provider store={store}>
+        <ResponseGQL />
+      </Provider>
+    );
+
+    expect(screen.getByText(`{"key": "value"}`)).toBeInTheDocument();
   });
 });
